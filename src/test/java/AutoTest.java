2403ea084cd0aaf5260c2ec51858794a58b7ecbd
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-04-10 20:53:11 +0000

    simplify test structure. AutoTests now passing.

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -11,13 +11,16 @@ import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import text.TestPrint;
 
 import static jas.utils.ColorFormatter.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static text.TestPrint.l;
 
@@ -29,263 +32,104 @@ import static text.TestPrint.l;
 @SuppressWarnings("unused")
 public class AutoTest {
     public static boolean WRITE = false;
-/*
-    public static void main(String args[]) throws Exception {
-        configureCAS();
-        Compiler.compile("((-1)*ln(x)+a)*(4+x)*cos(a)+(c+x)*sin(a)");
 
-        l(boldBlack("Updating expression library... this takes a while..."));
-        updateCandidates(
-//                args
-        );
-
-        String tests[] = new String[]{
-                "testSimplify",
-                "testSimplest",
-                "testNumNodes",
-                "testExpand",
-                "testToExponentialForm",
-                "testToAdditionOnly",
-                "testBeautify",
-                "testComplexity"
-        };
-        for (String test : tests) {
-            getMethod(AutoTest.class, test).invoke(null);
-        }
-    }
-
-    
-    private static void testComplexity() throws Exception {
-        l(lightPurple("\n---------> Complexity \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/complexity/bin_ops.txt", false, "complexity");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/complexity/u_ops.txt", false, "complexity");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/complexity/irr_num.txt", false, "complexity");
-    }
-
-
-    private static void testSimplify() throws Exception {
-        l(lightPurple("\n---------> Simplification \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/simplification/bin_ops.txt", true, "simplify");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/simplification/u_ops.txt", true, "simplify");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/simplification/irr_num.txt", true, "simplify");
-    }
-
-    private static void testSimplest() throws Exception {
-        l(lightPurple("\n---------> Simplest (Complex Simplification) \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/simplest/bin_ops.txt", true, "simplest");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/simplest/u_ops.txt", true, "simplest");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/simplest/irr_num.txt", true, "simplest");
-    }
-
-    private static void testBeautify() throws Exception {
-        l(lightPurple("\n---------> Beautification \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/beautify/bin_ops.txt", true, "simplify", "beautify");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/beautify/u_ops.txt", true, "simplify", "beautify");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/beautify/irr_num.txt", true, "simplify", "beautify");
-    }
-
-    private static void testNumNodes() throws Exception {
-        l(lightPurple("\n---------> Number of Nodes \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/nodes/bin_ops.txt", false, "numNodes");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/nodes/u_ops.txt", false, "numNodes");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/nodes/irr_num.txt", false, "numNodes");
-    }
-
-    private static void testExpand() throws Exception {
-        l(lightPurple("\n---------> Expansion \n"));
-        l(boldBlack("\n---------> Binary Operations Test \n"));
-        test("/tests/files/expansion/bin_ops.txt", true, "expand", "simplify");
-        l(boldBlack("\n---------> Unary Operations Test \n"));
-        test("/tests/files/expansion/u_ops.txt", true, "expand", "simplify");
-        l(boldBlack("\n---------> Irrational Numbers Test \n"));
-        test("/tests/files/expansion/irr_num.txt", true, "expand", "simplify");
-    }
-
-    
-*/
-
-    @SuppressWarnings("unchecked")
-    private static Method getMethod(Class c, String name) {
-        try {
-            return c.getDeclaredMethod(name);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        throw new RuntimeException("this exception will never happen");
-    }
-
-    private static ArrayList<String> getLines(String raw) {
-        ArrayList<String> lines = new ArrayList<>();
-        Collections.addAll(lines, raw.split("\n"));
-        return lines;
-    }
-
-    /**
-     * update the expressions in /additional, /exponential, /simplification according to ones listed in /expressions
-     */
-    private static void updateCandidates(String... subdirectories) {
-        ArrayList<ArrayList<String>> pool = new ArrayList<>();
-        String baseDir = "/tests/files/", ext = ".txt";
-        String[] types = new String[]{"bin_ops", "u_ops", "irr_num"};
-        for (String type : types) {
-            pool.add(getLines(Utils.read(baseDir + type + ext)));
-        }
-        for (String subDir : subdirectories) {
-            for (int m = 0; m < types.length; m++) {
-                String type = types[m];
-                String dir = baseDir + subDir + type + ext;
-                ArrayList<String> lines = getLines(Utils.read(dir));
-                for (String newLine : pool.get(m)) {
-                    if (!contains(lines, newLine)) {
-                        lines.add(getOriginal(newLine));
-                        l(lightGreen("\"" + getOriginal(newLine) + "\"") + " added to " + lightBlue(dir));
-                    }
-                }
-                writeLines(dir, lines);
-                l(dir + lightGreen(" ... UPDATED"));
-            }
-        }
-    }
-
-    private static boolean contains(ArrayList<String> lines, String s) {
-        for (String line : lines) {
-            String original = getOriginal(line);
-            try {
-                if (Compiler.compile(original).equals(Compiler.compile(getOriginal(s)))) {
-                    return true;
-                }
-            } catch (JASException e) {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    private static String getOriginal(String l) {
-        return (l.contains("->") ? l.substring(0, l.indexOf("->")) : l).trim();
-    }
-
-    private static String getComputed(String l) {
-        return (l.contains("->") ? l.substring(l.indexOf("->") + 2) : "").trim();
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "additional/bin_ops.txt",
+        "additional/irr_num.txt",
+        "additional/u_ops.txt",
+        "additional/u_der.txt"
+    })
+    public void test_toAdditionOnly(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.toAdditionOnly().toString());
     }
 
     @ParameterizedTest
-    @CsvSource(value = {
-        "/additional/bin_ops.txt,true,toAdditionOnly",
-        "/additional/u_ops.txt,true,toAdditionOnly",
-        "/additional/irr_num.txt,true,toAdditionOnly",
-        "/exponential/bin_ops.txt,true,toExponentialForm",
-        "/exponential/u_ops.txt,true,toExponentialForm",
-        "/exponential/irr_num.txt,true,toExponentialForm"
+    @CsvFileSource(delimiterString = "->",resources = {
+        "beautify/bin_ops.txt",
+        "beautify/irr_num.txt",
+        "beautify/u_ops.txt",
+        "beautify/u_der.txt"
     })
-    public void test(String fileName, boolean testValue, String methodsStr) throws Exception {
-        String []methods = methodsStr.split(",");
-        ArrayList<String> lines = getLines(Utils.read(fileName));
-        ArrayList<Node> ops = (ArrayList<Node>) lines.stream()
-                .map(l -> Compiler.compile(getOriginal(l)))
-                .collect(Collectors.toList());
-
-        ArrayList<String> simplifiedStrs = (ArrayList<String>) lines.stream()
-                .map(AutoTest::getComputed)
-                .collect(Collectors.toList());
-
-        int maxLength = 0;
-        for (Node op : ops) {
-            int l = op.toString().length();
-            maxLength = l > maxLength ? l : maxLength;
-        }
-        maxLength += 1;
-
-        final int finalMaxLength = maxLength;
-        lines = (ArrayList<String>) ops.stream()
-                .map(op -> ensureLength(op.toString(), finalMaxLength))
-                .collect(Collectors.toList());
-
-        for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i), prev = simplifiedStrs.get(i).replace(" ", "");
-            Object obj = ops.get(i).copy();
-            for (String method : methods) {
-                if (obj instanceof Node) {
-                    obj = getMethod(Node.class, method).invoke(obj);
-                } else break;
-            }
-
-            String now = obj.toString();
-            if (!prev.equals(now)) {
-                l(line + lightRed("CHANGED ")
-                        + lightBlue(prev) + lightRed(" ≠ ")
-                        + lightCyan(now));
-            }
-            if (testValue) {
-                Node o1 = Compiler.compile(line), o2 = Compiler.compile(now);
-                if (o1.numVars() > 0) {
-                    ArrayList<String> errs = new ArrayList<>();
-                    for (int k = 0; k <= 10; k++) {
-                        int t = (int) Math.pow(k, 2);                        
-                        double diffx = o1.eval(k) - o2.eval(k);
-                        if( Double.isNaN(diffx) ) {
-                            double first = o1.eval(k);
-                            double second = o1.eval(k);
-                            assertTrue(Double.isNaN(first) || Double.isInfinite(first),()->String.format("Value was %f",first));
-                            assertTrue(Double.isNaN(second) || Double.isInfinite(second),()->String.format("Value was %f",second));
-                        } else {
-                            assertTrue(diffx < 1E-10,() -> {
-                                StringBuilder sb = new StringBuilder();
-                                sb.append("For ").append(o1.toString()).append(" = ").append(o2.toString());
-                                sb.append(ensureLength("", finalMaxLength) + boldBlack("DIFF(x=" + t + "): ") + lightRed(diffx));
-                                return sb.toString();
-                            });
-                        }
-                        
-                        
-                    }
-                    
-                } else {
-                    double v = o1.val() - o2.val();
-                    if (v > 1E-10) {
-                        if (prev.equals(now))
-                            l(line + lightGreen("SAME") + boldBlack(" = ") + lightBlue(prev + " "));
-                        l(ensureLength("", maxLength) + boldBlack("RAW DIFF: ") + lightRed(v));
-                    }
-                }
-            }
-
-            lines.set(i, line + "-> " + now);
-        }
-        if (WRITE) writeLines(fileName, lines);
+    public void test_beautify(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.simplify().beautify().toString());
     }
 
-    private static void writeLines(String fileName, ArrayList<String> lines) {
-        Optional<String> content = lines.stream().map(l -> l + "\n").reduce((a, b) -> a + b);
-        content.ifPresent(c -> Utils.write(fileName, c));
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "complexity/bin_ops.txt",
+        "complexity/irr_num.txt",
+        "complexity/u_ops.txt",
+        "complexity/u_der.txt"
+    })
+    public void test_complexity(String input, Integer complexity) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(complexity,inputNode.complexity());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "nodes/bin_ops.txt",
+        "nodes/irr_num.txt",
+        "nodes/u_ops.txt",
+        "nodes/u_der.txt"
+    })
+    public void test_numberOfNodes(String input, Integer numberOfNodes) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(numberOfNodes,inputNode.numNodes());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "expansion/bin_ops.txt",
+        "expansion/irr_num.txt",
+        "expansion/u_ops.txt",
+        "expansion/u_der.txt"
+    })
+    public void test_expansion(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.expand().simplify().toString());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "exponential/bin_ops.txt",
+        "exponential/irr_num.txt",
+        "exponential/u_ops.txt",
+        "exponential/u_der.txt"
+    })
+    public void test_toExponential(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.toExponentialForm().toString());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "simplest/bin_ops.txt",
+        "simplest/irr_num.txt",
+        "simplest/u_ops.txt",
+        "simplest/u_der.txt"
+    })
+    public void test_simplest(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.simplest().toString());
+    }
+    
+    @ParameterizedTest
+    @CsvFileSource(delimiterString = "->",resources = {
+        "simplification/bin_ops.txt",
+        "simplification/irr_num.txt",
+        "simplification/u_ops.txt"
+    })
+    public void test_simplify(String input, String output) throws Exception {
+        Node inputNode = Compiler.compile(input);
+        assertEquals(output.trim(),inputNode.simplify().toString());
     }
 
     @BeforeAll
     public static void configureCAS() {
         Binary.define("%", 2, (a, b) -> a % b);
-    }
-
-    private static String ensureLength(String s, int length) {
-        StringBuilder sBuilder = new StringBuilder(s);
-        while (sBuilder.length() < length) {
-            sBuilder.append(" ");
-        }
-        s = sBuilder.toString();
-        return s;
     }
 }
